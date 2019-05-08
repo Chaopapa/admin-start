8bc3123fda29bdfb41a52d5cd4abcda4cada8fea
阿旺 <2785816783@qq.com>
2019-05-08 13:08:54 +0800

    删除多余引用

---
+++ le-log/src/main/java/com/le/log/aspect/SysLogAspect.java
@@ -0,0 +1,65 @@
+package com.le.log.aspect;
+
+import com.le.sso.service.ISSOService;
+import com.le.system.entity.SysUser;
+import com.le.system.service.ISysLogService;
+import com.le.web.annotation.SystemLog;
+import com.le.web.util.HttpContextUtils;
+import com.le.web.util.IPUtils;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author lz
+ * @ClassName SysLogAspect
+ * @description 系统日志，切面处理类
+ * @Version V1.0
+ * @since 2018/10/9 15:30
+ **/
+@Aspect
+@Component
+public class SysLogAspect {
+
+    @Autowired
+    private ISysLogService logService;
+    @Autowired
+    private ISSOService ssoService;
+
+    @Pointcut("@annotation(com.le.web.annotation.SystemLog)")
+    public void logPointCut() {
+
+    }
+
+    @Around("logPointCut()")
+    public Object around(ProceedingJoinPoint point) throws Throwable {
+        long beginTime = System.currentTimeMillis();
+        //执行方法
+        Object result = point.proceed();
+
+        MethodSignature signature = (MethodSignature) point.getSignature();
+        Method method = signature.getMethod();
+        SystemLog systemLog = method.getAnnotation(SystemLog.class);
+        String url = HttpContextUtils.getRequestUrl();
+        String ip = IPUtils.getIpAddr();
+
+        SysUser login = ssoService.findSystemLogin();
+        //执行时长(毫秒)
+        long time = System.currentTimeMillis() - beginTime;
+
+        if (login == null) {
+            logService.asyncLog(url, ip, null, null, null, systemLog.value(), time);
+        } else {
+            logService.asyncLog(url, ip, login.getId(), login.getUsername(), login.getName(), systemLog.value(), time);
+        }
+
+        return result;
+    }
+
+}
--- le-sso/src/main/java/com/le/sso/controller/UserAuthenticationContreoller.java
+++ le-sso/src/main/java/com/le/sso/controller/UserAuthenticationContreoller.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 /**
